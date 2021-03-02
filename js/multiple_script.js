4c0b17099399270ca015b91867543cad63f0aed7
Valeria Paz <21214@student.dorset-college.ie>
2021-03-02 12:24:25 +0000

    last commit

--- js/multiple_script.js
+++ js/multiple_script.js
@@ -1,28 +1,29 @@
 // Define a constructor function template for hotels
-function Hotel(name, rooms, booked) {
+function Hotel(name, rooms, booked, gym) {
     // Object properties
     this.name = name;
     this.rooms = rooms;
     this.booked = booked;
+    this.gym;
     this.checkAvailablity = function() {
         return this.rooms - this.booked;
     };
 }
 
 // Define to instances of the hotel and their arguments
-var clayHotel = new Hotel('Clayton', 68, 3);
-var lemonHotel = new Hotel('Lemon', 56, 15);
+var frozenHotel = new Hotel('Frozen Wilds', 55, 8, true);
+var horizonHotel = new Hotel('Horizon', 32, 11, false);
 
 // Declare a variable to hold the name of the hotel and room availability 
-var details1 = clayHotel.name + ' rooms: ';
+var details1 = frozenHotel.name + ' rooms: ';
     // add content to an existing variable----
-    details1 += clayHotel.checkAvailablity();
+    details1 += frozenHotel.checkAvailablity();
 // Get the hotel1 div
 var elHotel1 = document.getElementById('hotel1');
 // Add the variables created onto the empty div
 elHotel1.textContent = details1;
 
-var details2 = lemonHotel.name + ' rooms: ';
-    details2 += lemonHotel.checkAvailablity();
+var details2 = horizonHotel.name + ' rooms: ';
+    details2 += horizonHotel.checkAvailablity();
 var elHotel2 = document.getElementById('hotel2');
 elHotel2.textContent = details2;
--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 var hotel = {
     // Object properties
-    name: 'Clayton',
+    name: 'Frozen Wilds',
     rooms: 110,
-    booked: 25,
+    booked: 32,
     gym: true,
     roomTypes: ['double', 'twin', 'suite', 'king'],
     checkAvailablity: function() {
-        return this.rooms - this.booked;
+        return this.rooms - this.booked + this.gym;
     }
 };
 
 var elName = document.getElementById('hotelName');
 elName.textContent = hotel.name;
 
-var elRooms = document.getElementById('rooms');
+var elRooms = document.getElementById('rooms', 'gym');
 elRooms.textContent = hotel.checkAvailablity();
